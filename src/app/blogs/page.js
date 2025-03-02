b0665c09a5f28a62ff76bec55608ad025751140d
premnewbie <prempm50@gmail.com>
2025-03-02 18:04:43 +0530

    Change in the blog component

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,30 +1,32 @@
 import BlogOverview from "../components/blog-overview";
 import { BASE_API_URL } from "../utils/constants";
 
-async function fetchListOfBlogs(){
+async function fetchListOfBlogs() {
     try {
-        const apiResponse = await fetch(`${BASE_API_URL}/api/get-blogs`,{
+        const apiResponse = await fetch(`${BASE_API_URL}/api/get-blogs`, {
             method: 'GET',
             cache: 'no-store'
-        })
-
+        });
         const result = await apiResponse.json();
-    
-        return result?.data
+        return result?.data;
     } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
     }
 }
 
-export default async function Blogs(){
-
-    if(!BASE_API_URL){
-        return null;
+export async function getServerSideProps() {
+    if (!BASE_API_URL) {
+        return { props: { blogList: [] } };  // Fallback if the API URL is missing
     }
 
     const blogList = await fetchListOfBlogs();
+    return {
+        props: {
+            blogList
+        }
+    };
+}
 
-    return (
-        <BlogOverview blogList={blogList} />
-    )
+export default function Blogs({ blogList }) {
+    return <BlogOverview blogList={blogList} />;
 }
