861656265110845d9f1e5186a289f5899cf77f54
premnewbie <prempm50@gmail.com>
2025-03-02 18:08:28 +0530

    Changed to old version

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,33 +1,30 @@
 import BlogOverview from "../components/blog-overview";
 import { BASE_API_URL } from "../utils/constants";
 
-async function fetchListOfBlogs() {
+async function fetchListOfBlogs(){
     try {
-        const apiResponse = await fetch(`${BASE_API_URL}/api/get-blogs`, {
+        const apiResponse = await fetch(`${BASE_API_URL}/api/get-blogs`,{
             method: 'GET',
             cache: 'no-store'
-        });
+        })
+
         const result = await apiResponse.json();
-        return result?.data;
+    
+        return result?.data
     } catch (error) {
-        throw new Error(error);
+        throw new Error(error)
     }
 }
 
-export async function getStaticProps() {
-    if (!BASE_API_URL) {
-        return { props: { blogList: [] } };  // Fallback if the API URL is missing
+export default async function Blogs(){
+
+    if(!BASE_API_URL){
+        return null;
     }
 
     const blogList = await fetchListOfBlogs();
-    return {
-        props: {
-            blogList
-        },
-        revalidate: 60  // Re-fetch the data every 60 seconds in the background
-    };
-}
 
-export default function Blogs({ blogList }) {
-    return <BlogOverview blogList={blogList} />;
+    return (
+        <BlogOverview blogList={blogList} />
+    )
 }
